1a794677549438174bb55858a91fff5d6a5d1603
lubbock <Ayer1994@outlook.com>
2019-05-08 02:14:08 +0800

    fix:增加额外功能
    1. 自动探索bug修改

--- YysScript.py
+++ YysScript.py
@@ -1,13 +1,8 @@
-import pyautogui
 from Personal import *
-from scripts.v2.adventure import Adventure
-from scripts.heart import Heart
-from utils.Logger import Logger
-
-from utils.Monster import Monster
+from scripts.adventure import Adventure
 
 if __name__ == "__main__":
     log.info("游戏脚本启动")
-    # Heart.start(timer=15)
+    #Heart.start(timer=15)
     a = Adventure(exp=True, timer=100)
     a.start()
