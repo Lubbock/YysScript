042d2ef4c7019facc2dc3c536b34adfb7d6bc724
guoxj <Ayer1994@outlook.com>
2020-09-15 21:12:14 +0800

    fix: 事件修补
    事件代码修补

--- scripts/abnd3t.py
+++ scripts/abnd3t.py
@@ -2,6 +2,18 @@ from scripts.a_base import *
 import pyautogui
 from utils.winut import *
 from utils.MapCv import MapCv
+from decorator import decorator
+
+
+@decorator
+def win_location_screen(func, *args, **kwargs):
+    w, h = MapCv.location_screen(*args, **kwargs)
+    return w - 50, h - 50
+
+
+@win_location_screen
+def location_screen(*args, **kwargs):
+    log.info("location screen...")
 
 
 class Aband3t(Personal):
@@ -9,19 +21,19 @@ class Aband3t(Personal):
     @staticmethod
     def complete00000():
         log.info("开始事件Aband3t-0000")
-        w, h = MapCv.location_screen("Aband3t-0.png")
-        log.info(w, h)
-        pyautogui.moveTo(w - 40, h - 40)
-        pyautogui.click(w - 40, h - 40)
+        w, h = location_screen("Aband3t-0.png")
+        log.info(w)
+        pyautogui.moveTo(w, h)
+        pyautogui.click(w, h)
         log.info("结束事件Aband3t-0000")
 
     @staticmethod
     def complete0001(loopNum):
         log.info("开始事件Aband3t-0001")
-        w, h = MapCv.location_screen("Aband3t-1.png")
+        w, h = location_screen("Aband3t-1.png")
         log.info(w)
-        pyautogui.moveTo(w - 40, h - 40)
-        click(w - 40, h - 40, _pause=1, duration=1)
+        pyautogui.moveTo(w + 60, h)
+        click(w+60, h, _pause=1, duration=1)
         time.sleep(2)
         for i in range(loopNum):
             evt = ExitEvent(20)
@@ -29,8 +41,8 @@ class Aband3t(Personal):
                 evt.start_event()
                 log.info("evt leave ...-3")
                 time.sleep(2)
-            w, h = MapCv.location_screen("Aband3t-3.png")
-            click(w - 40, h - 40, _pause=1, duration=1)
+            w, h = location_screen("Aband3t-3.png")
+            click(w, h, _pause=1, duration=1)
             time.sleep(40)
             evt = ExitEvent(50)
             while not MapCv.__in_screenshot__("Aband3t-2.png"):
@@ -38,7 +50,7 @@ class Aband3t(Personal):
                 log.info("end... -2")
                 time.sleep(5)
             w, h = MapCv.location_screen("Aband3t-2.png")
-            click(w - 40, h - 40, _pause=1, duration=1)
+            click(w, h, _pause=1, duration=1)
             time.sleep(2)
 
         log.info("结束事件Aband3t-0001")
